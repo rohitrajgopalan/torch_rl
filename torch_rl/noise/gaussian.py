694f200249a5986467f0e3c19430b4047497146a
Rohit Gopalan <rohitgopalan@hotmail.com>
2021-06-09 22:03:36 +0930

    Create gaussian.py

---
+++ torch_rl/noise/gaussian.py
@@ -0,0 +1,14 @@
+class GaussianExploration:
+    def __init__(self, action_space, max_sigma=1.0, min_sigma=1.0, decay_period=1000000):
+        self.low  = action_space.low
+        self.high = action_space.high
+        self.max_sigma = max_sigma
+        self.min_sigma = min_sigma
+        self.decay_period = decay_period
+    
+    def get_action(self, action, t=0):
+        sigma  = self.max_sigma - (self.max_sigma - self.min_sigma) * min(1.0, t / self.decay_period)
+        action = action + np.random.normal(size=len(action)) * sigma
+        return np.clip(action, self.low, self.high)
+    
+#https://github.com/vitchyr/rlkit/blob/master/rlkit/exploration_strategies/gaussian_strategy.py
