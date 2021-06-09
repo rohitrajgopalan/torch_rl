65c5aebca3b1b81b29bea44b2f60349f0e21b0aa
Rohit Gopalan <rohitgopalan@hotmail.com>
2021-06-09 21:02:29 +0930

    Create setup.py

---
+++ setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup, find_packages
+
+setup(name='torch-rl',
+      version='0.1',
+      description='A Basic setup for Reinforcement Learning using PyTorch',
+      author='Rohit Gopalan',
+      author_email='rohitgopalan1990@gmail.com',
+      license='DST',
+      packages=find_packages(),
+      install_requires=['numpy', 'torch', 'gym'],
+      zip_safe=False)
