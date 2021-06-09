f4058b5d7535f9150e6ab1b5096a5ab7e87f9564
Rohit Gopalan <rohitgopalan1990@gmail.com>
2021-06-10 08:31:55 +0930

    Add all REINFORCE modules

---
+++ torch_rl/reinforce/agent.py
@@ -0,0 +1,53 @@
+import numpy as np
+import torch as T
+import torch.nn.functional as F
+from .network import Network
+
+
+class Agent:
+    def __init__(self, gamma, n_actions, input_dims,
+                 fc1_dims, fc2_dims, optimizer_type, optimizer_args={}):
+        self.gamma = gamma
+        self.policy = Network(input_dims, n_actions, fc1_dims, fc2_dims, optimizer_type, optimizer_args)
+        self.reward_memory = []
+        self.log_prob_memory = []
+        self.current_log_prob = 0.0
+        self.loss_history = []
+
+    def choose_action(self, observation):
+        state = T.Tensor([observation]).to(self.policy.device)
+        probabilities = F.softmax(self.policy.forward(state))
+        action_probs = T.distributions.Categorical(probabilities)
+        action = action_probs.sample()
+        log_probs = action_probs.log_prob(action)
+        self.log_prob_memory.append(log_probs)
+
+        return action.item()
+
+    def store_rewards(self, reward):
+        self.reward_memory.append(reward)
+
+    def learn(self):
+        self.policy.optimizer.zero_grad()
+
+        # G_t = R_t+1 + gamma * R_t+2 + gamma**2 * R_t+3
+        # G_t = sum from k=0 to k=T {gamma**k * R_t+k+1}
+        G = np.zeros_like(self.reward_memory, dtype=np.float64)
+        for t in range(len(self.reward_memory)):
+            G_sum = 0
+            discount = 1
+            for k in range(t, len(self.reward_memory)):
+                G_sum += self.reward_memory[k] * discount
+                discount *= self.gamma
+            G[t] = G_sum
+        G = T.tensor(G, dtype=T.float).to(self.policy.device)
+
+        loss = 0
+        for g, logprob in zip(G, self.action_memory):
+            loss += -g * logprob
+        loss.backward()
+        self.policy.optimizer.step()
+
+        self.action_memory = []
+        self.reward_memory = []
+        self.loss_history.append(loss.item())
---
+++ torch_rl/reinforce/main.py
@@ -0,0 +1,29 @@
+import numpy as np
+
+from .agent import Agent
+
+
+def run(env, n_games, gamma, fc1_dims, fc2_dims, optimizer_type, optimizer_args={}):
+    agent = Agent(gamma, env.action_space.n, env.observation_space.shape,
+                  fc1_dims, fc2_dims, optimizer_type, optimizer_args)
+
+    scores = np.zeros(n_games)
+
+    for i in range(n_games):
+        score = 0
+        observation = env.reset()
+        done = False
+
+        while not done:
+            action = agent.choose_action(observation)
+            observation_, reward, done, _ = env.step(action)
+            score += reward
+
+            agent.store_rewards(reward)
+            agent.learn()
+
+            observation = observation_
+
+        scores[i] = score
+
+    return np.mean(scores), np.mean(agent.loss_history)
