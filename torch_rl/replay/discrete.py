aac0f92499e317496634f09a3b4f1aac14cdc0e7
Rohit Gopalan <rohitgopalan@hotmail.com>
2021-06-21 15:34:57 +0930

    Add files via upload

--- torch_rl/replay/discrete.py
+++ torch_rl/replay/discrete.py
@@ -2,7 +2,7 @@ import numpy as np
 
 
 class DiscreteReplayBuffer:
-    def __init__(self, max_size, input_shape, randomized=False):
+    def __init__(self, max_size, input_shape, randomized=False, goal=None):
         self.mem_size = max_size
         self.mem_cntr = 0
         self.randomized = randomized
@@ -14,6 +14,11 @@ class DiscreteReplayBuffer:
         self.action_memory = np.zeros(self.mem_size, dtype=np.int64)
         self.reward_memory = np.zeros(self.mem_size, dtype=np.float32)
         self.terminal_memory = np.zeros(self.mem_size, dtype=np.bool)
+
+        if goal is None:
+            self.goal_memory = None
+        else:
+            self.goal_memory = np.full(shape=(self.mem_size, *input_shape), fill_value=goal, dtype=np.float32)
 
     def store_transition(self, state, action, reward, state_, done):
         index = self.mem_cntr % self.mem_size
@@ -38,4 +43,9 @@ class DiscreteReplayBuffer:
         states_ = self.new_state_memory[batch]
         terminal = self.terminal_memory[batch]
 
-        return states, actions, rewards, states_, terminal
+        if self.goal_memory is None:
+            goals = None
+        else:
+            goals = self.goal_memory[batch]
+
+        return states, actions, rewards, states_, terminal, goals
