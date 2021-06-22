8e7e06c1597aca124737feca4c9eef4d69c1cb69
Rohit Gopalan <rohitgopalan@hotmail.com>
2021-06-22 10:30:12 +0930

    Add files via upload

--- torch_rl/dqn/agent.py
+++ torch_rl/dqn/agent.py
@@ -1,13 +1,13 @@
 import numpy as np
 import torch as T
 from .network import Network
-from torch_rl.replay.discrete import DiscreteReplayBuffer
+from torch_rl.replay.replay import ReplayBuffer
 
 
 class Agent:
     def __init__(self, gamma, epsilon, n_actions, input_dims,
                  mem_size, batch_size, fc_dims, optimizer_type, eps_min=0.01, eps_dec=5e-7,
-                 replace=1000, optimizer_args={}, randomized=False):
+                 replace=1000, optimizer_args={}, randomized=False, goal=None):
         self.gamma = gamma
         self.epsilon = epsilon
         self.n_actions = n_actions
@@ -19,29 +19,42 @@ class Agent:
         self.action_space = [i for i in range(n_actions)]
         self.learn_step_counter = 0
 
-        self.memory = DiscreteReplayBuffer(mem_size, input_dims, randomized)
+        self.memory = ReplayBuffer(mem_size, input_dims, randomized=randomized, goal=goal)
 
-        self.q_eval = Network(self.input_dims, self.n_actions, fc_dims, optimizer_type, optimizer_args)
-
-        self.q_next = Network(self.input_dims, self.n_actions, fc_dims, optimizer_type, optimizer_args)
+        self.goal = goal
+        if self.goal is not None:
+            if not type(self.goal) == np.ndarray:
+                self.goal = np.array([self.goal]).astype(np.float32)
+            self.q_eval = Network(self.input_dims[0] + self.goal.shape[0], self.n_actions, fc_dims, optimizer_type,
+                                  optimizer_args)
+            self.q_next = Network(self.input_dims[0] + self.goal.shape[0], self.n_actions, fc_dims, optimizer_type,
+                                  optimizer_args)
+        else:
+            self.q_eval = Network(self.input_dims[0], self.n_actions, fc_dims, optimizer_type, optimizer_args)
+            self.q_next = Network(self.input_dims[0], self.n_actions, fc_dims, optimizer_type, optimizer_args)
 
         self.loss_history = []
 
     def choose_action(self, observation, train=True):
         if not train or np.random.random() > self.epsilon:
             if not type(observation) == np.ndarray:
-                observation = np.array([observation], dtype=np.float32)
+                observation = np.array([observation]).astype(np.float32)
             state = T.tensor([observation], dtype=T.float32).to(self.q_eval.device)
-            actions = self.q_eval.forward(state)
-            action = T.argmax(actions).item()
+            if self.goal is not None:
+                goal = T.tensor([self.goal], dtype=T.float32).to(self.q_eval.device)
+                inputs = T.cat([state, goal], dim=1)
+            else:
+                inputs = state
+            actions = self.q_eval.forward(inputs)
+            return T.argmax(actions).item()
         else:
-            action = np.random.choice(self.action_space)
+            return np.random.choice(self.action_space)
 
     def store_transition(self, state, action, reward, state_, done):
         self.memory.store_transition(state, action, reward, state_, done)
 
     def sample_memory(self):
-        state, action, reward, new_state, done = self.memory.sample_buffer(self.batch_size)
+        state, action, reward, new_state, done, goals = self.memory.sample_buffer(self.batch_size)
 
         states = T.tensor(state).to(self.q_eval.device)
         rewards = T.tensor(reward).to(self.q_eval.device)
@@ -49,7 +62,10 @@ class Agent:
         actions = T.tensor(action).to(self.q_eval.device)
         states_ = T.tensor(new_state).to(self.q_eval.device)
 
-        return states, actions, rewards, states_, dones
+        if goals is not None:
+            goals = T.tensor(goals).to(self.q_eval.device)
+
+        return states, actions, rewards, states_, dones, goals
 
     def replace_target_network(self):
         if self.learn_step_counter % self.replace_target_cnt == 0:
@@ -66,11 +82,19 @@ class Agent:
 
         self.replace_target_network()
 
-        states, actions, rewards, states_, dones = self.sample_memory()
+        states, actions, rewards, states_, dones, goals = self.sample_memory()
         indices = np.arange(self.batch_size)
 
-        q_pred = self.q_eval.forward(states)[indices, actions]
-        q_next = self.q_next.forward(states_).max(dim=1)[0]
+        if self.goal is not None:
+            goal = T.tensor([self.goal], dtype=T.float32).to(self.q_eval.device)
+            inputs = T.cat([states, goal], dim=1)
+            inputs_ = T.cat([states_, goal], dim=1)
+        else:
+            inputs = states
+            inputs_ = states_
+
+        q_pred = self.q_eval.forward(inputs)[indices, actions]
+        q_next = self.q_next.forward(inputs_).max(dim=1)[0]
 
         q_next[dones] = 0.0
         q_target = rewards + self.gamma * q_next
